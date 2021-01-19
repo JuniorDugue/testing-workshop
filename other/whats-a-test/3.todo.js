45dfa7dc1b9bf5b4bf78fb5a2450d989fab33557
juniordugue <jrdugue@gmail.com>
2021-01-19 13:53:59 -0500

    topics: comply eted the intro to testing covering automated testing, coding an assertion library & testing framework

--- other/whats-a-test/3.todo.js
+++ other/whats-a-test/3.todo.js
@@ -29,13 +29,27 @@ const {sum, subtract} = require('./math')
 
 let result, expected
 
-result = sum(3, 7)
-expected = 10
-expect(result).toBe(expected)
+test('sum adds numbers', () => {
+  const result = sum(3, 7)
+  const expected = 10
+  expect(result).toBe(expected)
+})
 
-result = subtract(7, 3)
-expected = 4
-expect(result).toBe(expected)
+test('subtract subtract numbers', () => {
+  const result = subtract(7, 3)
+  const expected = 4
+  expect(result).toBe(expected)
+})
+
+function test(title, callback) {
+  try {
+    callback()
+    console.log(`☑️ ${title}`)
+  } catch (error) {
+    console.log(`❌ ${title}`)
+    console.error(error)
+  }
+}
 
 function expect(actual) {
   return {
